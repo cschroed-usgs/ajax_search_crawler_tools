6585e540d333d89ed954cac896abf0b7e990f33c
Carl Schroedl <cschroedl@usgs.gov>
2015-09-08 17:26:40 -0500

    Migrating general functionality out of NWC project. First build passing.

---
+++ src/test/java/gov/usgs/cida/ajax_search_crawler_tools/mapper/PrettyUglyPair.java
@@ -0,0 +1,11 @@
+package gov.usgs.cida.ajax_search_crawler_tools.mapper;
+
+
+public class PrettyUglyPair {
+	public final String pretty;
+	public final String ugly;
+	public PrettyUglyPair(String pretty, String ugly){
+		this.pretty = pretty;
+		this.ugly = ugly;
+	}
+}
